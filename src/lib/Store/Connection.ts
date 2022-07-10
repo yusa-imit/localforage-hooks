5fceade3d4e3cdc6790f041b625069ff95b9d5f1
yusa-imit <esimileiar81@gmail.com>
2022-07-10 18:22:43 +0900

    change github ci

--- src/lib/Store/Connection.ts
+++ src/lib/Store/Connection.ts
@@ -1,10 +1,9 @@
 import JsStore from 'jsstore';
-import workerInjector from 'jsstore/dist/worker_injector';
-const getWorkerPath = () => {
-  return require('file-loader?name=scripts/[name].[hash].js!jsstore/dist/jsstore.worker.min.js');
-};
-const workerPath = getWorkerPath().default;
 
 export const Connection = window.Worker
-  ? new JsStore.Connection(new Worker(workerPath))
-  : new JsStore.Connection().addPlugin(workerInjector);
+  ? new JsStore.Connection(
+      new Worker(
+        require('file-loader?name=scripts/[name].[hash].js!jsstore/dist/jsstore.worker.min.js').default
+      )
+    )
+  : new JsStore.Connection().addPlugin(require('jsstore/dist/worker_injector'));
